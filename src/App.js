42ec713452c70807b7e8452ea0302eaea2fbe6a0
hwadii <wadii.hajji@etu.univ-amu.fr>
2019-11-09 15:56:22 +0100

    Bootrapped project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MoviesApp from "./MoviesApp";
+
+export class App extends React.Component {
+  render() {
+    return <MoviesApp />;
+  }
+}
+
+export default App;
